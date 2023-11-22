ab7b2534dbadcf15252c0bf4ddf61291c7cb976b
SuayMack <pry_chu@hotmail.com>
2023-11-21 23:12:24 -0300

    feat: created reset function, nivel level and sounds game over and error

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,61 +1,100 @@
 const state = {
   view: {
-    square: document.querySelectorAll(".square"),
+    squares: document.querySelectorAll(".square"),
     enemy: document.querySelector(".enemy"),
     timeLeft: document.querySelector("#time-left"),
     score: document.querySelector("#score"),
+    lives: document.querySelector("#live"),
   },
-  values: {    
+  values: {
     gameVelocity: 1000,
-    hitPosition: 0,
+    hitPosition: null,
     result: 0,
     currentTime: 60,
+    nivel: 1,
+    life: 3,
   },
   actions: {
-    timerId: setInterval(randomSquare, state.values.gameVelocity),
+    timerId: setInterval(randomSquare, 1000),
     countDownTimerId: setInterval(countDown, 1000),
   }
-};
-
-function main(){
-  addListenerHitBox();
 };
 
 function countDown() {
   state.values.currentTime--;
   state.view.timeLeft.textContent = state.values.currentTime;
+  state.view.lives.textContent = state.values.life;
+  state.view.score.textContent = state.values.result;
 
-  if (state.values.currentTime <= 0) {
-    clearInterval(state.actions.timerId);
-    clearInterval(state.actions.countDownTimerId);
-    alert("GAME OVER" + "\n" + "YOUR SCORE: " + state.values.result);
-    
-  };
+  if(state.values.result < 0){
+    if(state.values.life > 0){
+      state.values.life--;
+      state.values.currentTime = 60;
+      alert("Você pedeu 1 vida 😢");  
+      state.values.result = 0;
+    } else if( state.values.life == 0){
+      alert("Game Over");
+      playSound("gameover");
+      resetGame();
+    }
+  }
 
+  if (state.values.currentTime == 0 && state.values.life > 0) {
+    let nextLevel = state.values.nivel++;
+    alert("Nível " + nextLevel + "\n" + "YOUR SCORE: " + state.values.result);
+    state.values.gameVelocity += 500;    
+    state.values.currentTime = 60;
+  } else if (state.values.currentTime === 0 || state.view.lives == 0) {
+    alert("Game Over" + "\n" + "YOUR SCORE: " + state.values.result);
+    playSound("gameover");
+    resetGame();
+  }
+}
+
+function resetGame() {
+  state.values.currentTime = 60;
+  state.view.score.innerHTML = 0;
+  state.view.lives.innerHTML = 3;
+  state.values.life = 3;
+  state.values.result = 0;
+}
+
+function playSound(audioName) {
+  let audio = new Audio(`./src/sounds/${audioName}.m4a`);
+  audio.volume = 0.2;
+  audio.play();
 }
 
 function randomSquare() {
-  state.view.square.forEach((square) => {
+  state.view.squares.forEach((square) => {
     square.classList.remove("enemy");
   })
 
   let randomNumber = Math.floor(Math.random() * 9);
-  let randomSquare = state.view.square[randomNumber];
+  let randomSquare = state.view.squares[randomNumber];
   randomSquare.classList.add("enemy");
   state.values.hitPosition = randomSquare.id;
 }
 
 function addListenerHitBox() {
-  state.view.square.forEach((square) => {
+  state.view.squares.forEach((square) => {
     square.addEventListener("mousedown", () => {
+      state.view.score.textContent = state.values.result;
       if (square.id === state.values.hitPosition) {
-        state.values.result ++;
-        state.view.score.textContent = state.values.result;
+        state.values.result++;
         //para limpar a posição do inimigo
         state.values.hitPosition = null;
+        playSound("hit");
+      } else {
+        playSound("error");
+        state.values.result--;
       }
     });
   });
 }
+
+function main() {
+  addListenerHitBox();
+};
 
 main();
